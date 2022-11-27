ca754781e0cd416653086a55c39e75bc4e7171f4
Ianca Lisandra Uehara Xavier <ueharalisandra@gmail.com>
2022-11-27 18:52:16 -0300

    menu com o destaque quando selecionado

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function mostrarAtivo(local){
+    let li = document.getElementById('header-menu');
+    let a = li.getElementsByTagName('a');
+    for (i=0; i<a.length; i++ ){
+       a[i].style.color = "";
+    }
+       local.style.color = "#d7d7d7";
+    }
+
+
+//função de destaque do menu
