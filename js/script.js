dec89d4a838686501ccaaae851863a00fc952805
Ianca Lisandra Uehara Xavier <ueharalisandra@gmail.com>
2022-11-30 16:23:03 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -11,3 +11,28 @@ function mostrarAtivo(local){
 //função de destaque do menu
 
 
+const section = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .container ul li');
+
+window.addEventListener('scroll', ()=>{
+   let current = '';
+
+   section.forEach( section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if(pageYoffset >= (sectionTop - sectionHeight /3)){
+         current = section.getAttribute('id');
+         
+      }
+   })
+
+   navLi.forEach( (li) => {
+
+      li.classList.remove('active');
+      if(li.classList.contains(current)){
+         li.classList.add('active');
+      }
+   })
+})
+
+
